f952eae87c148824db530884b721f1318b635c43
Evgenija <sotiroska.e@gmail.com>
2025-01-19 13:13:18 +0100

    adding microservices

---
+++ CompanyIssuerService/src/main/java/com/example/companyissuerservice/CompanyIssuerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.companyissuerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyIssuerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyIssuerServiceApplication.class, args);
+	}
+
+}
---
+++ CompanyIssuerService/src/test/java/com/example/companyissuerservice/CompanyIssuerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.companyissuerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyIssuerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
